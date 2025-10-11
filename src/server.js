b08ed99ac81f760aa5f2373d45e2dc344526252a
nbrucepro <nezabruce@gmail.com>
2025-10-11 11:03:25 +0800

    binding-gateway

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ async function startServer() {
     // await prisma.$connect();
     console.log("Database connected");
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });    
   } catch (err) {
     console.error("❌ Database connection failed:", err);
     process.exit(1);
